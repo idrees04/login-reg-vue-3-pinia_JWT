19ac5e01bf9d56faa852018b6942d6b7eaccffc5
idrees04 <idrees04@yahoo.com>
2022-10-31 13:47:19 +0500

    add all

---
+++ login-reg-vue-3-pinia/src/views/account/Layout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6
+       offset-sm-2 mt-5">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
